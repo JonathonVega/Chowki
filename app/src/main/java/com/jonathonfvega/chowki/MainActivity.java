83fe9f143ee14be3bec2b1f352d7b062c18c9a09
Jonathon Vega <jfvega@live.com>
2017-04-01 09:00:51 -0400

    Added Firebase dependency, but google services needs to be fixed

--- app/src/main/java/com/jonathonfvega/chowki/MainActivity.java
+++ app/src/main/java/com/jonathonfvega/chowki/MainActivity.java
@@ -1,13 +1,56 @@
 package com.jonathonfvega.chowki;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String TAG = "Cool";
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button buttonSignUp = (Button) findViewById(R.id.signIn);
+        buttonSignUp.setOnClickListener(this);
+    }
+
+    public void registerUser() {
+        mAuth.createUserWithEmailAndPassword("123@domain.com", "password")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG);
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        registerUser();
     }
 }
