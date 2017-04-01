1c8f5c0870e341f49ccb11dd678b0d7b4949beff
Jonathon Vega <jfvega@live.com>
2017-04-01 11:27:43 -0400

    Able to get device's phone number

--- app/src/main/java/com/jonathonfvega/chowki/MainActivity.java
+++ app/src/main/java/com/jonathonfvega/chowki/MainActivity.java
@@ -1,8 +1,10 @@
 package com.jonathonfvega.chowki;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +14,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -31,17 +35,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void registerUser() {
+        String userPhoneNumber = getPhoneNumber();
         mAuth.createUserWithEmailAndPassword("123@domain.com", "password")
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG);
 
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference myRef = database.getReference("message");
+
+                        myRef.setValue("Hello, World!");
+
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
                         if (!task.isSuccessful()) {
-
+                            Toast.makeText(MainActivity.this, "Failure", Toast.LENGTH_LONG);
                         }
 
                         // ...
@@ -52,5 +62,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         registerUser();
+    }
+
+    public String getPhoneNumber() {
+        TelephonyManager tMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        String mPhoneManager = tMgr.getLine1Number();
+        System.out.print(mPhoneManager);
+        return mPhoneManager;
     }
 }
